ecb2fd639957854c4e473dd3176a7185281e764c
dirgepye <s.stenhouse87@gmail.com>
2016-03-25 06:47:19 +0000

    basic deck created, outlines for basic functions

---
+++ script.js
@@ -0,0 +1,85 @@
+//CARDS
+
+var deck = [
+    {
+        key: "twoHearts",
+        num: 2,
+        suit: "heart"
+    },
+    {
+        key: "threeHearts",
+        num: 3,
+        suite: "heart"
+    },
+        {
+        key: "fourHearts",
+        num: 4,
+        suit: "heart"
+    },
+    {
+        key: "fiveHearts",
+        num: 5,
+        suite: "heart"
+    },
+        {
+        key: "sixHearts",
+        num: 6,
+        suit: "heart"
+    },
+    {
+        key: "sevenHearts",
+        num: 7,
+        suite: "heart"
+    },
+        {
+        key: "eightHearts",
+        num: 8,
+        suit: "heart"
+    },
+    {
+        key: "nineHearts",
+        num: 9,
+        suit: "heart"
+    },
+    {
+        key: "tenHearts",
+        num: 10,
+        suit: "heart"
+    },
+    {
+        key: "jackHearts",
+        num: 11,
+        suite: "heart"
+    },
+        {
+        key: "queenHearts",
+        num: 12,
+        suit: "heart"
+    },
+    {
+        key: "kingHearts",
+        num: 13,
+        suite: "heart"
+    },
+    {
+        key: "aceHearts",
+        num: 13,
+        suite: "heart"
+    },
+    ];
+
+
+var yourHand = []
+var compHand =[]
+
+function deal() {
+    //deal cards to yourHand and compHand
+    
+    //should prevent duplicates
+}
+
+function fire() {
+    //set two cards down
+}
+
+console.log(deck.length)
