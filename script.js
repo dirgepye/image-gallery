a1df124f23337f4fb2acc88b3834c259ea3ed9ca
dirgepye <s.stenhouse87@gmail.com>
2016-05-04 00:23:05 +0000

    deal cards, show cards. counter stops working eventually seemingly at random :(

--- script.js
+++ script.js
@@ -1,147 +1,109 @@
 //CARDS
 
+
 var yourHand = []
 var compHand = []
 
 var deck = [];
 
-function Card(num, suit) {
+function Card(num, suit, key) {
     this.num = num;
     this.suit = suit;
+    this.key = key;
 }
 
 
-for(var i = 0; i < 52; i++) {
+for (var i = 0; i <= 51; i++) {
     var num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     var value = num[Math.floor(Math.random() * num.length)];
-    
-    var type = ["Heart", "Diamond", "Spade", "Club"];
+
+    var type = ["heart", "diamond", "spade", "club"];
     var suit = type[Math.floor(Math.random() * type.length)];
     
-    deck[i] = new Card(value, suit)
-    
-    //document.getElementById("app").innerHTML = "<p>Suit is " + deck[i].suit + " and it has a value of " + deck[i].num + "</p>"
-    //$("#app").append("<p>Suit is " + deck[i].suit + " and it has a value of " + deck[i].num + "</p>")
-    
-    deck.forEach(verify);
-    
-    function verify() {
-    for (var y = 0; y = deck.length; y++) {
-        if (deck[i].suit == deck[y].suit && deck[i].num == deck[y].num) {
-            console.log(deck[i] + "matches with" + deck[y])
+    var key = value + suit;
+    deck[i] = new Card(value, suit, key)
+
+    for (var j = 0; j < deck.length; j++) {
+
+        console.log("j is " + j)
+        
         }
-    }
-    }
+        
+        // if (deck[i].key == deck[j].key) {
+        //     console.log("match")
+    //}
 }
 
-console.log("lookin at " + deck[5].suit)
 
+function deal() {
+    for (var h = 0; h < 26; h++) {
+        yourHand.push(deck[h]);
+    }
+
+    for (var x = 26; x < 52; x++) {
+        compHand.push(deck[x]);
+    }
+
+    console.log("your deck has " + yourHand.length + " cards")
+    console.log(yourHand)
+    console.log("the computer has " + compHand.length + " cards")
+    console.log(compHand)
+
+    document.getElementById("your-hand").innerHTML = "<p>You have " + yourHand.length + " cards in your hand</p>";
+    document.getElementById("comp-hand").innerHTML = "<p>The computer has " + compHand.length + "cards in its hand</p>";
+    document.getElementById("deal").innerHTML = "Fire!";
+    document.getElementById("deal").onclick = function() {
+        fire();
+    }
+
+
+
+}
+
+function fire() {
+    var hCard = yourHand[0];
+    var cCard = yourHand[0];
+    
+    //var hCard = Math.floor(Math.random() * yourHand.length)
+    //var cCard = Math.floor(Math.random() * compHand.length)
+
+    if (yourHand[hCard].num > compHand[cCard].num) {
+        compHand.splice(cCard, 1);
+        yourHand.push(compHand[cCard]);
+        console.log("you win");
+        console.log(yourHand);
+        console.log(compHand);
+    }
+    else if (yourHand[hCard].num < compHand[cCard].num) {
+        yourHand.splice(hCard, 1);
+        compHand.push(yourHand[hCard]);
+        console.log("you lose");
+        console.log(yourHand);
+        console.log(compHand);
+    }
+    else {
+        console.log("tie!")
+    }
+
+    document.getElementById("your-hand").innerHTML = "<p>You have " + yourHand.length + " cards in your hand</p><p>object number " + hCard + "<p style='font-size: 30px'>" + yourHand[hCard].num + "</p><img src='" + yourHand[hCard].suit + ".png' width='20px'>"
+
+    document.getElementById("comp-hand").innerHTML = "<p>The computer has " + compHand.length + " cards in their hand</p><p>object number " + cCard + "<p style='font-size: 30px'>" + compHand[cCard].num + "</p><img src='" + compHand[cCard].suit + ".png' width='20px'>"
+
+
+}
+
+console.log("you push " + yourHand)
+
+console.log("your deck has " + yourHand.length + " cards")
 console.log(deck)
 console.log(deck.length)
 
-// function num() {
-//     var num = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-//     var value = num[Math.floor(Math.random() * num.length)];
-//     //var numRes = this.value;
-//     return value;
-// }
 
-// function suit() {
-//     var type = ["Heart", "Diamond", "Spade", "Club"];
-//     var suit = type[Math.floor(Math.random() * type.length)];
-//     return suit;
-// };
+// html test
+
+var y = Math.floor((Math.random() * deck.length) + 1);
+console.log(y)
 
 
 
-
-// var theTree = new Tree('Redwood');
-// console.log('theTree.constructor is ' + theTree.constructor);
-
-// var i = 1;
-// do {
-//     var testDeck = [];
-//     var yourCard = new card;
-//     console.log(yourCard);
-//     testDeck.push(yourCard);
-//     i++ ;
-    
-// }
-// while (i < 5);
-
-
-
-
-
-
-
-
-
-// var card1 = new card;
-// var card4 = new card;
-// var card6 = new card;
-// var card7 = new card;
-
-// console.log(card7)
-
-// testDeck.push(card1,card4,card6)
-
-// console.log(card1);
-
-//console.log(testDeck);
-
-
-
-    // for (i = 0; i < 6; i++) {
-    //     yourHand[i] = deck[i].key;
-    //     //deck.splice[i];
-    //     console.log(yourHand)
-    //     console.log(deck)
-    // }
-
-    // for (i = 0; i < 12; i++) {
-    //     compHand = deck[i].key;
-    //     console.log("comps hand " + compHand)
-    // }
-    //deal cards to yourHand and compHand
-    //should prevent duplicates
-
-
-
-
-// var warrior1 = new warrior("steve","male");
-// var warrior2 = new warrior("alf","male");
-// var warrior3 = new warrior("barbara walters","female");
-// var warrior4 = new warrior("hillary clinton","female");
-// var warrior5 = new warrior("george clooney","male");
-// var warrior6 = new warrior("henry kissinger","male")
-
-// console.log(warrior1.faceoff(warrior4));
-
-// console.log(warrior2);
-
-// var deck = []
-
-// deck.push(warrior2,warrior3)
-
-// console.log(deck)
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// var testDeck = [1,2,3,4];
-// testDeck.push({deal});
-
-// var card = new deal();
-
-// console.log(card);
-
-
-// console.log(testDeck);
-
-//console.log(deal());
-
-function fire() {
-    //set two cards down
-}
-
-//console.log(deck.length)
+//document.getElementById("app").innerHTML = "<img src='" + deck[y].suit + ".png' width='100px'>"
